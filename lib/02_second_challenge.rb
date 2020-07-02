78629bc7e4baf58791205e790cfb628447343b5a
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 14:44:35 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,17 +1,14 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
+epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :heroine => {}
+      :heroine => {},
       :heroine_friends => []
-      }}
-
-  
-end
+   }
+}
